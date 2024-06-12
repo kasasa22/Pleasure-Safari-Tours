caf8dbafd46c6565841dc875f4fe9b372d8d4f32
Trevor22 <kasasatrevor25@gmail.com>
2024-06-12 14:03:44 +0300

    about-us

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,21 +3,20 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import '../styles.css'; 
-import headerimg from '../assets/img/breadcrumb.png'; 
 
 const Header = () => {
   return (
-    <div className="container-fluid bg-breadcrumb" style={{backgroundImage: `url(${headerimg})`, backgroundSize: 'cover', backgroundPosition: 'center'}}>
+    <div className="container-fluid bg-breadcrumb">
       <div className="container text-center py-5" style={{ maxWidth: '900px' }}>
-        <h3 className="text-white display-3 mb-4 wow fadeInDown" data-wow-delay="0.1s">Our Services</h3>
+        <h3 className="text-purple-800 display-3 mb-4 wow fadeInDown" data-wow-delay="0.1s">Our Services</h3>
         <ol className="breadcrumb justify-content-center text-white mb-0 wow fadeInDown" data-wow-delay="0.3s">
           <li className="breadcrumb-item">
-            <a href="index.html" className="text-white">Home</a>
+            <a href="index.html" className="text-warning">Home</a>
           </li>
           <li className="breadcrumb-item">
-            <a href="#" className="text-white">Pages</a>
+            <a href="#" className="text-warning">Pages</a>
           </li>
-          <li className="breadcrumb-item active text-secondary">Services</li>
+          <li className="breadcrumb-item active text-warning">Services</li>
         </ol>
       </div>
     </div>
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -16,26 +16,26 @@ const About = () => {
             <div className="col-xl-5 wow fadeInLeft" data-wow-delay="0.1s">
               <div className="bg-light rounded">
                 <img src={about} className="img-fluid w-100" style={{ marginBottom: '-7px' }} alt="Image" />
-                <img src={about1} className="img-fluid w-100 border-bottom border-5 border-primary" style={{ borderTopRightRadius: '300px', borderTopLeftRadius: '300px' }} alt="Image" />
+                <img src={about1} className="img-fluid w-100 border-bottom border-5 border-purple" style={{ borderTopRightRadius: '300px', borderTopLeftRadius: '300px' }} alt="Image" />
               </div>
             </div>
             <div className="col-xl-7 wow fadeInRight" data-wow-delay="0.3s">
-              <h5 className="sub-title pe-3">About the company</h5>
+              <h5 className="sub-title pe-3 text-purple">About the company</h5>
               <h1 className="display-5 mb-4">We’re Trusted Tours and Travel Agency.</h1>
-              <p className="mb-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt architecto consectetur iusto perferendis blanditiis assumenda dignissimos, commodi fuga culpa earum explicabo libero sint est mollitia saepe! Sequi asperiores rerum nemo!</p>
+              <p className="mb-4">At <span className='text-purple'>Preasure and Safari </span>Tours, we specialize in crafting unforgettable travel experiences. From breathtaking adventures to relaxing getaways, our expertly curated tours cater to all types of travelers. Discover hidden gems, immerse yourself in diverse cultures, and create lasting memories with our personalized travel packages. Whether you’re seeking the thrill of exploration or the comfort of guided tours, we’re here to make your dream vacation a reality. Join us and embark on a journey of a lifetime!</p>
               <div className="row gy-4 align-items-center">
                 <div className="col-12 col-sm-6 d-flex align-items-center">
-                  <i className="fas fa-map-marked-alt fa-3x text-secondary"></i>
+                  <i className="fas fa-map-marked-alt fa-3x text-purple"></i>
                   <h5 className="ms-4">Best Travel Resources</h5>
                 </div>
                 <div className="col-12 col-sm-6 d-flex align-items-center">
-                  <i className="fas fa-passport fa-3x text-secondary"></i>
+                  <i className="fas fa-passport fa-3x text-purple"></i>
                   <h5 className="ms-4">Explore the country</h5>
                 </div>
                 <div className="col-4 col-md-3">
                   <div className="bg-light text-center rounded p-3">
                     <div className="mb-2">
-                      <i className="fas fa-ticket-alt fa-4x text-primary"></i>
+                      <i className="fas fa-ticket-alt fa-4x text-purple"></i>
                     </div>
                     <h1 className="display-5 fw-bold mb-2">10</h1>
                     <p className="text-muted mb-0">Years of Experience</p>
@@ -43,9 +43,9 @@ const About = () => {
                 </div>
                 <div className="col-8 col-md-9">
                   <div className="mb-5">
-                    <p className="text-primary h6 mb-3"><i className="fa fa-check-circle text-secondary me-2"></i> Offer 100 % Genuine Assistance</p>
-                    <p className="text-primary h6 mb-3"><i className="fa fa-check-circle text-secondary me-2"></i> It’s Faster & Reliable Execution</p>
-                    <p className="text-primary h6 mb-3"><i className="fa fa-check-circle text-secondary me-2"></i> Accurate & Expert Advice</p>
+                    <p className="text-primary h6 mb-3"><i className="fa fa-check-circle text-purple me-2"></i> <span className="text-warning">Offer 100 % Genuine Assistance</span></p>
+                    <p className="text-primary h6 mb-3"><i className="fa fa-check-circle text-purple me-2"></i> <span className="text-warning">It’s Faster & Reliable Execution</span></p>
+                    <p className="text-primary h6 mb-3"><i className="fa fa-check-circle text-purple me-2"></i><span className="text-warning"> Accurate & Expert Advice</span></p>
                   </div>
                   <div className="d-flex flex-wrap">
                     <div id="phone-tada" className="d-flex align-items-center justify-content-center me-4">
