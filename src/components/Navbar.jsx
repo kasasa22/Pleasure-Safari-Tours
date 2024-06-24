f050259204835a795cefdf0c2008fb608149bf38
Trevor22 <kasasatrevor25@gmail.com>
2024-06-24 14:47:55 +0300

    nav

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,15 +9,19 @@ const Navbar = () => {
     setIsOpen(!isOpen);
   };
 
+  const closeMenu = () => {
+    setIsOpen(false);
+  };
+
   return (
     <nav className="bg-purple-950 border-b border-indigo-500">
       <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
         <div className="flex h-20 items-center justify-between">
-          <div className="flex flex-1 items-center justify-center md:items-stretch md:justify-start">
+          <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start guide-icon rounded-pill p-2">
             {/* Logo */}
-            <a className="flex flex-shrink-0 items-center mr-4" href="/index.html">
-              <img className="h-10 w-auto md:hidden" src={logo} alt="React Jobs" />
-              <span className=" text-white text-2xl font-bold ml-2">
+            <a className="flex flex-shrink-0  mr-4" href="/index.html">
+              {/* <img className="h-10 w-auto md:hidden" src={logo} alt="React Jobs" /> */}
+              <span className="text-white text-2xl font-bold ml-2">
                 Pleasure & Safari Tours
               </span>
             </a>
@@ -27,55 +31,60 @@ const Navbar = () => {
                 className="text-white focus:outline-none"
               >
                 &#9776; 
-                              </button>
+              </button>
             </div>
-            <div className={`md:ml-auto ${isOpen ? 'block' : 'hidden'} md:flex space-x-2`}>
+            <div className={`absolute top-16 left-0 right-0 bg-purple-950 md:static md:bg-transparent ${isOpen ? 'block' : 'hidden'} md:flex md:ml-auto space-y-2 md:space-y-0 md:space-x-2`}>
               <NavLink
                 to="/"
+                onClick={closeMenu}
                 className={({ isActive }) =>
                   isActive
-                    ? 'text-white bg-black hover:bg-gray-900 hover:text-white rounded-md px-3 py-2'
-                    : 'text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2'
+                    ? 'text-white bg-black hover:bg-gray-900 hover:text-white rounded-md px-3 py-2 sm:bg-transparent sm:hover:bg-gray-900 sm:text-gray-200'
+                    : 'text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2 sm:bg-transparent sm:hover:bg-gray-900 sm:text-gray-200'
                 }
               >
                 Home
               </NavLink>
               <NavLink
                 to="/aboutUs"
+                onClick={closeMenu}
                 className={({ isActive }) =>
                   isActive
-                    ? 'text-white bg-black hover:bg-gray-900 hover:text-white rounded-md px-3 py-2'
-                    : 'text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2'
+                    ? 'text-white bg-black hover:bg-gray-900 hover:text-white rounded-md px-3 py-2 sm:bg-transparent sm:hover:bg-gray-900 sm:text-gray-200'
+                    : 'text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2 sm:bg-transparent sm:hover:bg-gray-900 sm:text-gray-200'
                 }
               >
                 About
               </NavLink>
               <NavLink
                 to="/services"
+                onClick={closeMenu}
                 className={({ isActive }) =>
                   isActive
-                    ? 'text-white bg-black hover:bg-gray-900 hover:text-white rounded-md px-3 py-2'
-                    : 'text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2'
+                    ? 'text-white bg-black hover:bg-gray-900 hover:text-white rounded-md px-3 py-2 sm:bg-transparent sm:hover:bg-gray-900 sm:text-gray-200'
+                    : 'text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2 sm:bg-transparent sm:hover:bg-gray-900 sm:text-gray-200'
                 }
               >
                 Services
               </NavLink>
               <NavLink
                 to="/Blogs"
+                onClick={closeMenu}
                 className={({ isActive }) =>
                   isActive
-                    ? 'text-white bg-black hover:bg-gray-900 hover:text-white rounded-md px-3 py-2'
-                    : 'text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2'
+                    ? 'text-white bg-black hover:bg-gray-900 hover:text-white rounded-md px-3 py-2 sm:bg-transparent sm:hover:bg-gray-900 sm:text-gray-200'
+                    : 'text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2 sm:bg-transparent sm:hover:bg-gray-900 sm:text-gray-200'
                 }
               >
                 Blogs
               </NavLink>
               <NavLink
                 to="/contact"
+                onClick={closeMenu}
                 className={({ isActive }) =>
                   isActive
-                    ? 'text-white bg-black hover:bg-gray-900 hover:text-white rounded-md px-3 py-2'
-                    : 'text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2'
+                    ? 'text-white bg-black hover:bg-gray-900 hover:text-white rounded-md px-3 py-2 sm:bg-transparent sm:hover:bg-gray-900 sm:text-gray-200'
+                    : 'text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2 sm:bg-transparent sm:hover:bg-gray-900 sm:text-gray-200'
                 }
               >
                 Contact
