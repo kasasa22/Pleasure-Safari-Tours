de491999a5f50b62e575d83d99d70dd5ffa7b9b3
Trevor22 <kasasatrevor25@gmail.com>
2024-06-24 15:18:42 +0300

    components

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import logo from '../assets/images/logo.png';
 import { NavLink } from 'react-router-dom';
 
+
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
 
@@ -17,20 +18,19 @@ const Navbar = () => {
     <nav className="bg-purple-950 border-b border-indigo-500">
       <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
         <div className="flex h-20 items-center justify-between">
-          <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start guide-icon rounded-pill p-2">
+          <div className="flex flex-1 items-center justify-between sm:items-stretch sm:justify-start guide-icon rounded-pill p-2">
             {/* Logo */}
-            <a className="flex flex-shrink-0  mr-4" href="/index.html">
-              {/* <img className="h-10 w-auto md:hidden" src={logo} alt="React Jobs" /> */}
+            <a className="flex flex-shrink-0 mr-4 website-name" href="/index.html">
               <span className="text-white text-2xl font-bold ml-2">
                 Pleasure & Safari Tours
               </span>
             </a>
-            <div className="md:hidden">
+            <div className="md:hidden menu-icon">
               <button
                 onClick={toggleMenu}
                 className="text-white focus:outline-none"
               >
-                &#9776; 
+                &#9776;
               </button>
             </div>
             <div className={`absolute top-16 left-0 right-0 bg-purple-950 md:static md:bg-transparent ${isOpen ? 'block' : 'hidden'} md:flex md:ml-auto space-y-2 md:space-y-0 md:space-x-2`}>
